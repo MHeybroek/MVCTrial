070541e0e8080f441906274b0b589a0f290417c7
Mats Heybroek <Matsheybroek@gmail.com>
2020-04-05 20:18:13 +0200

    Changed naming for React application to .React and added an Razor Page Application

--- MVCTrial.React/Program.cs
+++ MVCTrial.React/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace MVCTrial.RazorPages
+namespace MVCTrial.ReactFormat
 {
     public class Program
     {
